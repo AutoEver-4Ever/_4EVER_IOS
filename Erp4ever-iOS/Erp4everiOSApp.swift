23d0b126e612bc08b10972e74c6bb74c6dd77364
OhChangEun <dwc07109@gmail.com>
2025-09-29 17:29:56 +0900

    feat: 프로젝트 초기 설정

---
+++ Erp4ever-iOS/Erp4everiOSApp.swift
@@ -0,0 +1,18 @@
+//
+//  Erp4everiOSApp.swift
+//  Erp4everiOSApp
+//
+//  Created by OhChangEun on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Erp4everiOSApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
