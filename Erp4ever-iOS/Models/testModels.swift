9b96975fd414af2fdde3c258e79b771b2df33e7e
k-oyun <113766051+k-oyun@users.noreply.github.com>
2025-10-27 21:04:38 +0900

    #1 feat(main): mvvm 구조 설정

---
+++ Erp4ever-iOS/Models/testModels.swift
@@ -0,0 +1,8 @@
+//
+//  testModels.swift
+//  Erp4ever-iOS
+//
+//  Created by Admin on 10/27/25.
+//
+
+import Foundation
---
+++ Erp4ever-iOS/Utils/testUtils.swift
@@ -0,0 +1,8 @@
+//
+//  testUtils.swift
+//  Erp4ever-iOS
+//
+//  Created by Admin on 10/27/25.
+//
+
+import Foundation
